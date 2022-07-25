102bf038f343aa41982810c75d0522f40dfa32a2
ilyass <elmofid.ilyass@gmail.com>
2022-07-25 17:03:54 +0100

    Implementation de Customer Service

---
+++ src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,26 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(new Customer(null , "Ilyass" , "ilyass@gmail.com"));
+            customerRepository.save(new Customer(null , "Mohamed" , "mohamed@gmail.com"));
+            customerRepository.save(new Customer(null , "Ali" , "ali@gmail.com"));
+            customerRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
